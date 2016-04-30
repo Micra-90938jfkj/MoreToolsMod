48fe2bb5426c812df973f9c869e94a2c9ed3d79e
unknown <>
2016-04-30 15:18:26 +0800

    Added Star rock and Star rock fragment

---
+++ src/main/java/micra/moreTools/blocks/BlockStarRock.java
@@ -0,0 +1,7 @@
+package micra.moreTools.blocks;
+
+/**
+ * Created by Administrator on 2016/4/30.
+ */
+public class BlockStarRock {
+}
---
+++ src/main/java/micra/moreTools/items/ItemStarRockFragment.java
@@ -0,0 +1,7 @@
+package micra.moreTools.items;
+
+/**
+ * Created by Administrator on 2016/4/30.
+ */
+public class ItemStarRockFragment {
+}
